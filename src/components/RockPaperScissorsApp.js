f843ea0660a9c633dbd2f888bfc00f25569a6845
Awilda I Couverthier <awildacouverthier@gmail.com>
2020-03-28 16:04:15 -0400

    add component logic to main component

--- src/components/RockPaperScissorsApp.js
+++ src/components/RockPaperScissorsApp.js
@@ -7,9 +7,11 @@ export default class RockPaperScissorsApp extends React.Component {
 		choices: ['Rock', 'Paper', 'Scissors'],
 		chosen: '',
 		computer: '',
-		message: 'You Win!'
+		message: 'You Lose!'
 	};
+	this.handleSubmit = this.handleSubmit.bind(this);
 	}
+
 	randomChoice = () => {
 		const randomPick = Math.floor(Math.random() * this.state.choices.length);
 		const pick = this.state.choices[randomPick];
@@ -18,26 +20,54 @@ export default class RockPaperScissorsApp extends React.Component {
 			computer: pick 
 		}));
 
-		console.log(pick);
+		if(this.randomChoice) {
+			return this.findWinner();
+		}
 	};
-	findWinner = (randomChoice) => {
-		console.log("findWinner");
-		console.log(this.state.chosen);
+
+	findWinner = () => {
+		const playerChoice = this.state.chosen;
+		const computerChoice = this.state.computer;
+
+		if (playerChoice == 'Rock' && computerChoice == 'Scissors' ||
+			playerChoice == 'Paper' && computerChoice == 'Rock' ||
+			playerChoice == 'Scissors' && computerChoice == "Paper") {
+			
+			this.setState(() => ({ 
+				message: 'You Win!' 
+			}));
+		
+		} else if (playerChoice == 'Rock' && computerChoice == 'Rock' ||
+				playerChoice == 'Paper' && computerChoice == 'Paper' ||
+				playerChoice == 'Scissors' && computerChoice == 'Scissors') {
+
+			this.setState(() => ({ 
+				message: 'It\'s A Tie!' 
+			}));
+		}
+
 	};
+
+	handleSubmit = chosen => {
+		this.setState({ chosen });
+		console.log(chosen);
+		if(chosen !== '') {
+			return this.randomChoice();
+		}
+	};	
 
   render() {
     return (
     	<div>
       		<Header />
-      		<Choices choices={this.state.choices} chosen={this.handleSubmit} />
-      		<ComputerChoice 
-      			randomChoice={this.randomChoice} 
-      			computer={this.state.computer}
-      		/>
-      		<GetWinningPlayer 
-      			findWinner={this.findWinner}
-      			message={this.state.message}
-      		/>
+      		{this.state.choices.map((choice, index) =>
+				<button 
+					key={choice}
+					type="button"
+					value={choice}
+					onClick={e => this.handleSubmit(e.target.value)}
+				>{choice}
+				</button>)}
       	</div>
     );
   }
@@ -47,67 +77,7 @@ const Header = () => {
 	return (
 		<div>
 			<h1>Rock, Paper, Scissors sayyysss...</h1>
+			<h3>Choose your pick!</h3>
 		</div>
 	);
-}
-
-class Choices extends React.Component {
-	constructor(props) {
-		super(props);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
-	handleSubmit = chosen => {
-		this.setState({ chosen });
-		console.log(chosen);
-	}	
-	render() {
-		return(
-			<div>
-				{this.props.choices.map((choice, index) =>
-					<button 
-						key={choice}
-						type="button"
-						value={choice}
-						onClick={e => this.handleSubmit(e.target.value)}
-					>{choice}
-					</button>)}
-			</div>
-		);
-	}
-}
-
-class ComputerChoice extends React.Component {
-	constructor(props) {
-		super(props);
-		this.randomChoice = props.randomChoice.bind(this);
-	}
-	render() {
-		return (
-			<div>
-				<button
-					onClick={e => this.randomChoice(e.target.value)}
-					>
-						randomChoice
-				</button>
-			</div>
-		);
-	}
-}
-
-class GetWinningPlayer extends React.Component {
-	constructor(props) {
-		super(props);
-		this.findWinner = props.findWinner.bind(this);
-	}
-	render() {
-		return (
-			<div>
-				<button
-					onClick={e => this.findWinner(e.target.value)}
-					>
-						findWinner
-				</button>
-			</div>
-		);
-	}
 }
